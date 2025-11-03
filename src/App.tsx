bcd00b0d768178d0d9ee87f3839e3b00caba129d
nataly-33 <vanessamartinez1@upb.edu>
2025-11-03 19:36:45 -0400

    chore(frontend): agregar punto de entrada, estilos y assets públicos

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { AppRoutes } from "@core/routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
